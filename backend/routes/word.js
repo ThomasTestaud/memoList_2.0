7eefe6c487e9bd520ad40fd7d792e5f133512ff4
*Stratt321963 <thomas.testaud@hotmail.fr>
2023-09-27 09:13:48 +0200

    backend stuff

--- backend/routes/word.js
+++ backend/routes/word.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+let list = [
+  {
+    id: 1,
+    list_id: 1,
+    string_one: "Bonjour",
+    string_two: "Hello"
+  },
+]
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.json("coucou");

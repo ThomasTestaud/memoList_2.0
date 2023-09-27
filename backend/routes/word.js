07f7c1373f696dbc0816bc0f1fdcfef36775ac83
*Stratt321963 <thomas.testaud@hotmail.fr>
2023-09-27 11:48:51 +0200

    finished the crud for word

--- backend/routes/word.js
+++ backend/routes/word.js
@@ -7,16 +7,40 @@ router.get('/', function(req, res, next) {
   const query = `SELECT * FROM words`;
 
   SQLquery(query, (error, response) => {
-    if (error) {
-      console.error('Error executing SQL query:', error);
-      return res.status(500).json({ error: 'Internal server error' });
-    }
-    
-    if (!response || response.length === 0) {
-      return res.status(404).json({ message: 'No data found' });
-    }
-
     res.json(response);
+  });
+});
+
+router.post('/', function(req, res, next) {
+  const stringOne = req.body.stringOne;
+  const stringTwo = req.body.stringTwo;
+  
+  const query = "INSERT INTO `words`(`list_id`, `string_one`, `string_two`) VALUES ('"+1+"','"+stringOne+"','"+stringTwo+"')";
+
+  SQLquery(query, (error, response) => {
+    res.json("success");
+  });
+});
+
+router.patch('/:id', function(req, res, next) {
+  const id = Number(req.params.id);
+  const stringOne = req.body.stringOne;
+  const stringTwo = req.body.stringTwo;
+  
+  const query = "UPDATE `words` SET `string_one`='"+stringOne+"',`string_two`='"+stringTwo+"' WHERE id = "+id+";";
+
+  SQLquery(query, (error, response) => {
+    res.json("success");
+  });
+});
+
+router.delete('/:id', function(req, res, next) {
+  const id = Number(req.params.id);
+  
+  const query = "DELETE FROM `words` WHERE id = "+id+";";
+
+  SQLquery(query, (error, response) => {
+    res.json("success");
   });
 });
 

bb8a835c2aa4c51ba06cd36a88eeaf82166a4384
*Stratt321963 <thomas.testaud@hotmail.fr>
2023-09-27 15:56:35 +0200

    ...

--- frontend/js/list.js
+++ frontend/js/list.js
@@ -1,10 +1,23 @@
 const url = 'http://localhost:3000/list/';
 const validate = document.getElementById('validate');
 const allLists = document.getElementById('all-lists');
+const editPopup = document.getElementById('edit-popup');
+const cancelEdit = document.getElementById('cancel-edit');
+const validateEdit = document.getElementById('validate-edit');
+let listData;
 
 validate.addEventListener('click', () => {
     createList();
 })
+
+cancelEdit.addEventListener('click', () => {
+    editPopup.classList.add('none');
+})
+validateEdit.addEventListener('click', () => {
+    editPopup.classList.add('none');
+})
+
+
 
 getLists();
 
@@ -55,6 +68,7 @@ function getLists() {
       .then(responseData => {
         console.log('Response Data:', responseData);
         displayLists(responseData);
+        listData = responseData;
       })
       .catch(error => {
         console.error('Fetch error:', error);
@@ -90,7 +104,8 @@ function displayLists(data) {
     for(list of data) {
         allLists.innerHTML += `
         <div class="card m-2 position-relative">
-            <svg class="trash position-absolute t-0 s-75 h-25 z-3" data-id="${list.id}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" /></svg>      
+            <svg style="position:absolute; top:10px; right: 10px; height: 20px; cursor:pointer;" class="trash text-danger" data-id="${list.id}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" /></svg>      
+            <svg style="position:absolute; top:10px; right: 40px; height: 20px; cursor:pointer;" class="pen text-warning" data-id="${list.id}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" /></svg>
             <a href="/word.php?listId=${list.id}" class="card-body text-decoration-none">
                 <h2 class="card-title">${list.name}</h2>
                 <p class="card-text">${list.description}</p>
@@ -102,12 +117,31 @@ function displayLists(data) {
     mapEventsOnCards();
 }
 
+function openPopUp(id) {
+    editPopup.classList.remove('none');
+
+    for(list of listData) {
+        if(list.id === id) {
+            console.log(list.name);
+        }
+        console.log(id);
+        console.log(list.id);
+    }
+}
+
 function mapEventsOnCards() {
-    const cards = document.querySelectorAll('.trash');
-    cards.forEach((el, index) =>  {
+    const trashs = document.querySelectorAll('.trash');
+    trashs.forEach((el, index) =>  {
         el.addEventListener('click', () => {
             console.log(el.dataset.id)
             deleteList(el.dataset.id);
+        })
+    })
+    const pen = document.querySelectorAll('.pen');
+    pen.forEach((el, index) =>  {
+        el.addEventListener('click', () => {
+            console.log(el.dataset.id)
+            openPopUp(el.dataset.id);
         })
     })
 }
--- frontend/list.php
+++ frontend/list.php
@@ -1,20 +1,59 @@
 <?php require_once("header.php"); ?>
 
-<div>
-    <h1>Create List</h1>
-    <label for="title">List's name:</label>
-    <input type="text" id="title">
-    <label for="description">List's name:</label>
-    <input type="text" id="description">
-    <button id="validate">Validate</button>
+<div class="container">
+  <h1 class="mt-4">Create new List</h1>
+    <div class="form-group">
+      <label for="title">List's name:</label>
+      <input type="text" class="form-control" id="title">
+    </div>
+    <div class="form-group">
+      <label for="description">List's description:</label>
+      <input type="text" class="form-control" id="description">
+    </div>
+    <button type="button" class="btn btn-success mt-1 mb-4" id="validate">Validate</button>
 </div>
-<div id="all-lists">
-
+<div id="all-lists" style="margin: auto; max-width: 500px;">
+</div>
+<div id="edit-popup" class="none">
+    <div id="edit-form">
+    <div class="form-group">
+      <label for="title">Modify name:</label>
+      <input type="text" class="form-control" id="title">
+    </div>
+    <div class="form-group">
+      <label for="description">Modify description:</label>
+      <input type="text" class="form-control" id="description">
+    </div>
+    <div class="d-flex justify-content-center">
+        <button id="validate-edit" class="btn btn-success m-1 mt-2">Validate</button>
+        <button id="cancel-edit" class="btn btn-warning m-1 mt-2">Cancel</button>
+    </div>
+    </div>
 </div>
 <style>
     /* CSS for the hover effect */
     .card:hover {
         background-color: #b7b7b7;
+    }
+
+    #edit-popup {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0,0,0,0.7);
+        transform: translate(-50%,-50%);
+    }
+
+    #edit-form {
+        margin: auto;
+        margin-top:30vh;
+        max-width: 500px;
+    }
+
+    .none {
+        display: none;
     }
 </style>
 

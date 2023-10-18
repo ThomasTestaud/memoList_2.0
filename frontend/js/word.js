f3f2120aaabdce86f6901aa5cc4fcc395ca6fd45
mponsramells <mponsramells@gmail.com>
2023-10-18 13:54:49 +0200

    ...

--- frontend/js/word.js
+++ frontend/js/word.js
@@ -2,7 +2,8 @@
 // Create a URL object from the current URL
 const url = new URL(window.location.href);
 const idList = Number(url.searchParams.get('listId'));
-const apiUrl = 'http://localhost:3000/word/'+ idList;
+const apiUrl = 'http://localhost:3005/word/'+ idList;
+console.log(apiUrl);
 const string_one = document.getElementById('string-one');
 const string_two_response = document.getElementById('string-two-response');
 const show_answer = document.getElementById('show-answer');
@@ -30,10 +31,40 @@ fetch(apiUrl)
         string_one.innerHTML = data[randomIndex].string_one
         string_two_response.innerHTML = data[randomIndex].string_two;
         trueResponse = data[randomIndex].string_two;
-        // You can process the data here or update your UI
+        const tableBody = document.querySelector("table tbody");
+        data.forEach(item => {
+            const newRow = document.createElement("tr");
+            const stringOneInput = document.createElement("input");
+            stringOneInput.type = "text";
+            stringOneInput.className = "form-control";
+            stringOneInput.value = item.string_one;
+            const stringTwoInput = document.createElement("input");
+            stringTwoInput.type = "text";
+            stringTwoInput.className = "form-control";
+            stringTwoInput.value = item.string_two;
+            const listIdInput = document.createElement("input");
+            listIdInput.type = "text";
+            listIdInput.className = "form-control";
+            listIdInput.value = item.list_id;
+            const stringOneCell = document.createElement("td");
+            stringOneCell.appendChild(stringOneInput);
+            const stringTwoCell = document.createElement("td");
+            stringTwoCell.appendChild(stringTwoInput);
+            const listIdCell = document.createElement("td");
+            const actionsCell = document.createElement("td");
+            actionsCell.innerHTML =
+                `<svg style="height: 20px; cursor:pointer;" class="trash text-danger" data-id="data.id" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" /></svg>
+                <svg style="height: 20px; cursor:pointer;" class="pen text-warning" data-id="data.id" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" /></svg>          
+            `
+            listIdCell.appendChild(listIdInput);
+            newRow.appendChild(stringOneCell);
+            newRow.appendChild(stringTwoCell);
+            newRow.appendChild(listIdCell);
+            newRow.appendChild(actionsCell);
+            tableBody.appendChild(newRow);
+        });
     })
     .catch(error => {
-        // Handle any errors that occurred during the fetch
         console.error('Fetch error:', error);
     });
 
